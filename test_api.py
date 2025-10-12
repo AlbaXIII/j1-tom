3ce8375768be879c0831fb94da01ceb7d4db280d
AlbaXIII <alex.jbaker20@googlemail.com>
2025-10-12 15:56:02 +0100

    feat - successful API connection test & added club badges

--- test_api.py
+++ test_api.py
@@ -1,3 +1,16 @@
 import requests
 import os
 from dotenv import load_dotenv
+
+load_dotenv()
+
+# Replace with your actual API key
+API_KEY = os.getenv("API_SPORTS_KEY")
+
+url = 'https://v3.football.api-sports.io/status'
+headers = {
+    'x-apisports-key': API_KEY
+}
+
+response = requests.get(url, headers=headers)
+print(response.json())
